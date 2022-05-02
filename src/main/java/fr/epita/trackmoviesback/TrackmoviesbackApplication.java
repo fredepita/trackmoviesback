cdb9fcecc31e30a47b1eac66ca57319f2092bb30
fred_epita <frederic.fang@epita.fr>
2022-05-02 11:48:46 +0200

    First import

---
+++ src/main/java/fr/epita/trackmoviesback/TrackmoviesbackApplication.java
@@ -0,0 +1,13 @@
+package fr.epita.trackmoviesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmoviesbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmoviesbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/epita/trackmoviesback/TrackmoviesbackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epita.trackmoviesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmoviesbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
