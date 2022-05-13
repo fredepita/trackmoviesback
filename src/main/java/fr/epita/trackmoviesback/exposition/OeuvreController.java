b76f07ba85e89d1a88831de1ccfd88d4bac6233a
fabienSAYA <fsayatravail@hotmail.com>
2022-05-13 14:17:12 +0200

    gestion des parametres de filtre dans le controller

--- src/main/java/fr/epita/trackmoviesback/exposition/OeuvreController.java
+++ src/main/java/fr/epita/trackmoviesback/exposition/OeuvreController.java
@@ -6,7 +6,9 @@ import fr.epita.trackmoviesback.dto.OeuvreLightListDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/trackmovies/v1")
@@ -16,8 +18,17 @@ public class OeuvreController {
     OeuvreService service;
 
     @GetMapping("/mes_oeuvres")
-    OeuvreLightListDto getAllOeuvres(){
-        return service.getAllOeuvres();
+    OeuvreLightListDto getOeuvres(@RequestParam(name = "type") String typeOeuvre
+            , @RequestParam(name = "genre") Integer genreId
+            , @RequestParam(name = "statut") Integer statutVisionnageId
+            , @RequestParam(name = "titre") String titre
+    ){
+        Map<String,String> parameters = new HashMap<>();
+        parameters.put("type",typeOeuvre);
+        parameters.put("genre", String.valueOf(genreId));
+        parameters.put("statut",String.valueOf(statutVisionnageId));
+        parameters.put("titre",titre);
+        return service.getOeuvres(parameters);
     }
 
 
